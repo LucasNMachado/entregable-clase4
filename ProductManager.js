46d06cd609f74cca83b398be2e7d0d34922792c2
Lucas <machadolucasn@gmail.com>
2023-05-23 16:45:31 -0300

    correccion 23/5

--- ProductManager.js
+++ ProductManager.js
@@ -1,79 +1,67 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-      this.products = [];
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    addProduct(product) {
-      const newProduct = { ...product, id: this.generateId() };
-      this.products.push(newProduct);
-    }
-  
-    getProductById(productId) {
-      const product = this.products.find((product) => product.id === productId);
-      if (!product) {
-        throw new Error("Producto no encontrado");
-      }
-      return product;
-    }
-  
-    updateProduct(productId, updatedFields) {
-      const product = this.getProductById(productId);
-      const updatedProduct = { ...product, ...updatedFields };
-      const index = this.products.findIndex((product) => product.id === productId);
-      this.products[index] = updatedProduct;
-    }
-  
-    deleteProduct(productId) {
-      const index = this.products.findIndex((product) => product.id === productId);
-      if (index === -1) {
-        throw new Error("Producto no encontrado");
-      }
-      this.products.splice(index, 1);
-    }
-  
-    generateId() {
-      let id;
-      do {
-        id = Math.random().toString(36).substr(2, 9);
-      } while (this.products.some((product) => product.id === id));
-      return id;
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.products = this.loadProducts();
+  }
+
+  loadProducts() {
+    try {
+      const fileData = fs.readFileSync(this.filePath, 'utf-8');
+      return JSON.parse(fileData);
+    } catch (error) {
+      return [];
     }
   }
-  
 
-  const productManager = new ProductManager();
-  
-  // duvuelve un arreglo vacío []
-  console.log(productManager.getProducts()); // 
-  // agregar un producto 
-  productManager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25
-  });
-  
- // deberia mostrar el id
-  console.log(productManager.getProducts()); 
-  
-  
-  const productId = // id que queres obtener
-  console.log(productManager.getProductById(productId)); // muestra el id
-  
-  
-  const productIdToUpdate = // id del producto que queres actualizar
-  const updatedFields = { 
-       // campo que queres actualizar
-  };
-  productManager.updateProduct(productIdToUpdate, updatedFields);
-  
-  
-  const productIdToDelete = // id que queres eliminar
-  productManager.deleteProduct(productIdToDelete);
-  
+  saveProducts() {
+    const jsonData = JSON.stringify(this.products, null, 2);
+    fs.writeFileSync(this.filePath, jsonData);
+  }
+
+  addProduct(product) {
+    const newProduct = { ...product, id: this.generateId() };
+    this.products.push(newProduct);
+    this.saveProducts();
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(productId) {
+    const product = this.products.find((product) => product.id === productId);
+    if (!product) {
+      throw new Error('Producto no encontrado');
+    }
+    return product;
+  }
+
+  updateProduct(productId, updatedFields) {
+    const product = this.getProductById(productId);
+    const updatedProduct = { ...product, ...updatedFields };
+    const index = this.products.findIndex((product) => product.id === productId);
+    this.products[index] = updatedProduct;
+    this.saveProducts();
+  }
+
+  deleteProduct(productId) {
+    const index = this.products.findIndex((product) => product.id === productId);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+    this.products.splice(index, 1);
+    this.saveProducts();
+  }
+
+  generateId() {
+    let id;
+    do {
+      id = Math.random().toString(36).substr(2, 9);
+    } while (this.products.some((product) => product.id === id));
+    return id;
+  }
+}
+
+module.exports = ProductManager;
